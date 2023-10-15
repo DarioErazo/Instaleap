185a4cd38f92b76b4ad143ebbe07ed6ca77e4514
DarioErazo <nanchodario@gmail.com>
2023-10-15 12:26:56 -0400

    first version to clean code

--- app.py
+++ app.py
@@ -1,53 +1,51 @@
-from flask import Flask, render_template, request, jsonify
-from flask_session import Session
-import requests as req
+from flask import Flask, render_template, request
 
-from external_requests import get_avaibility, create_job, details_job, update_json, update_job
+from external_requests import (
+    get_availability, 
+    create_job, 
+    details_job,
+    update_job
+)
 
 app = Flask(__name__)
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+
 
 @app.route('/')
-def index():
+def index() -> str:
+    """redirect to home page"""
     return render_template('home.html')
 
 
-@app.route('/slots', methods = ['POST', 'GET'])
-def slot_view():
-    if request.method == "POST":
-        response = get_avaibility()
-        return render_template("availability_table.html", data=response)
+@app.route('/slots', methods = ['POST'])
+def slot_view() -> str:
+     """Get available slots"""
+     response = get_availability()
+     return render_template("availability_table.html", data=response)
+
       
 @app.route('/jobs', methods = ["POST"])
-def def_job():
+def def_job() -> str:
+        """Create Jobs"""
         slot_id = request.form.get('slot_id')
-        client_id = request.form.get('client_id')
         response = create_job(slot_id)
         return render_template("job_created.html", data=response)
 
-@app.route('/job/search', methods = ['POST', 'GET'])
-def search():
-     return render_template("Job_search_client.html")
-     
+
 @app.route('/jobs/details/<string:job_id>', methods = ["GET"])
-def details(job_id):
+def details(job_id) -> str:
+     """List job details"""
      response = details_job(job_id)
      response["payment_info"]["id"] = job_id
      return render_template("details.html", data=response, items=response["items"])
 
+
 @app.route('/jobs/update/<string:job_id>', methods = ["POST"])
-def update(job_id):
+def update(job_id) -> str:
+     """Update payment job"""
+     #TODO: amount no puede ir vacio
      amount = request.form.get('amount')
      job_info = request.form.get('payment_info')
-     job_number, data = update_json(amount, job_info)
-     print("-------  Imprimir desde el principal  -------")
-     print(data)
-     response = update_job(job_number, data)
-     print("-------  Imprimiendo EL RESPONSE  -------")
-     print(response)
-     print(type(response))
-     #print(response.status_code)
+     response = update_job(job_info, amount)
      return render_template("payment_details.html", data=response)
 
 
